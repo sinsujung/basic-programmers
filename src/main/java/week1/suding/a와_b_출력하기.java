840bc135fdf6cb1590fbcacf62fecd8935a0fd92
suding <jhw02045@naver.com>
2024-03-13 02:02:27 +0900

    Create-181951: a와 b 출력하기

---
+++ src/main/java/week1/suding/a와_b_출력하기.java
@@ -0,0 +1,14 @@
+package week1.suding;
+
+import java.util.Scanner;
+
+public class a와_b_출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("a = %d \n", a);
+        System.out.printf("b = %d", b);
+    }
+}
